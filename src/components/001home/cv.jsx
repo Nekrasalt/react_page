7e3c7ccd653121cc4cd654689cb3c43ac4804926
Nekrasalt <Nekrasalt@users.noreply.github.com>
2022-07-07 19:30:31 +0530

    my all commit 5

--- src/components/001home/cv.jsx
+++ src/components/001home/cv.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/Copy.pdf'
 const cv = () => {
     return (
         <div className="cvap cta">
-            <a href='https://nekrasalt.github.io/Portfolio/index.html' download className='btn btn_down'>Download CV</a>
+            <a href='https://nekrasalt.github.io/Portfolio/index.html' className='btn btn_down'>Download CV</a>
             <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
     )
