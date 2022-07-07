0e448348fee18ce58b64a7160d63c41d0ed60511
Nekrasalt <91146947+Nekrasalt@users.noreply.github.com>
2022-07-07 15:06:48 +0530

    Update footer.jsx

--- src/components/05 footer/footer.jsx
+++ src/components/05 footer/footer.jsx
@@ -19,10 +19,10 @@ const footer = () => {
                 <li><a href="#testimonials">Testimonials</a></li>
             </ul>
             <div className="footer_socials">
-                <a href="https://linkedin.com" target='_blank'><FiLinkedin /></a>
-                <a href="https://twitter.com" target='_blank'><FiTwitter /></a>
-                <a href="https://youtube.com" target='_blank'><FiYoutube /></a>
-                <a href="https://instagram.com" target='_blank'><FiInstagram /></a>
+                <a href="https://www.linkedin.com/in/kuldeep-sethiya-a222b71b2/" target='_blank'><FiLinkedin /></a>
+                <a href="https://twitter.com/KuldeepSethiya" target='_blank'><FiTwitter /></a>
+                <a href="https://www.youtube.com/channel/UC3DoVxfPBqxyrjI8Abw6aWQ" target='_blank'><FiYoutube /></a>
+                <a href="https://www.instagram.com/nekrasalt/" target='_blank'><FiInstagram /></a>
                 <a href="https://tinder.com" target='_blank'><TbBrandTinder /></a>
             </div>
             <div className="footer_copyright">
